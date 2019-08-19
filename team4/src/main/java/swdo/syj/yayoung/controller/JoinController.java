78cc7879cb5d9faa75238c1127152b6a70ed796c
user <user@DESKTOP-01ND8VV>
2019-08-20 00:49:21 +0900

    nn

--- team4/src/main/java/swdo/syj/yayoung/controller/JoinController.java
+++ team4/src/main/java/swdo/syj/yayoung/controller/JoinController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,8 @@ import swdo.syj.yayoung.dao.JoinDao;
 @Controller
 @RequestMapping("join")
 public class JoinController {
-
+	
+	@Autowired
 	JoinDao dao;
 	
 	final String uploadPath_tc = "/memberfile/teacher";	//선생님 파일 업로드 경로
